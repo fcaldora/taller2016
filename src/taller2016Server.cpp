48f41b95e991d266054cbbce72e52bf7181503d7
peragu <agustinperrotta_31@hotmail.com>
2016-03-27 16:18:46 -0300

    al recibir mensaje lo encolo en una lista. Hay un thread corriendo (se q esta corriendo porque cuando le mando un cout dentro del while llena la pantalla) que deberia desencolar. el tema es q le paso la lista y nunca imprime porque el if(!list.empty()) nunca entra. Ta para revisar eso.

--- src/taller2016Server.cpp
+++ src/taller2016Server.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <sys/unistd.h>
 #include <string.h>
+#include <list>
+#include <pthread.h>
 #define MAXHOSTNAME 256
 using namespace std;
 
@@ -29,29 +31,43 @@ int readBlock(int fd, void* buffer, int len) {
 	return count;
 }
 
+void* readMsjs(void *param) {
+	clientMsj msj = { };
+	list<clientMsj> msjs = *((list<clientMsj> *) param);
+	while (1) {
+		if (!msjs.empty()) {
+			msj = msjs.front();
+			cout << msj.id << endl;
+			cout << msj.value << endl;
+			cout << msj.type << endl;
+			msjs.pop_front();
+		}
+	}
+	return 0;
+}
+
 int main() {
 
+	list<clientMsj> msjs;
 	struct sockaddr_in socketInfo;
-	char sysHost[MAXHOSTNAME+1];  // Hostname of this computer we are running on
+	char sysHost[MAXHOSTNAME + 1]; // Hostname of this computer we are running on
 	struct hostent *hPtr;
 	int socketHandle;
 	int portNumber = 8080;
 
-	bzero(&socketInfo, sizeof(sockaddr_in));// Clear structure memory
+	bzero(&socketInfo, sizeof(sockaddr_in));  // Clear structure memory
 
 	// Get system information
 
-	gethostname(sysHost, MAXHOSTNAME);// Get the name of this computer we are running on
-	if((hPtr = gethostbyname(sysHost)) == NULL)
-	{
+	gethostname(sysHost, MAXHOSTNAME); // Get the name of this computer we are running on
+	if ((hPtr = gethostbyname(sysHost)) == NULL) {
 		cerr << "System hostname misconfigured." << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	// create socket
 
-	if((socketHandle = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
+	if ((socketHandle = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		close(socketHandle);
 		exit(EXIT_FAILURE);
 	}
@@ -59,31 +75,35 @@ int main() {
 	// Load system information into socket data structures
 
 	socketInfo.sin_family = AF_INET;
-	socketInfo.sin_addr.s_addr = htonl(INADDR_ANY);// Use any address available to the system
-	socketInfo.sin_port = htons(portNumber);// Set port number
+	socketInfo.sin_addr.s_addr = htonl(INADDR_ANY); // Use any address available to the system
+	socketInfo.sin_port = htons(portNumber);  // Set port number
 
 	// Bind the socket to a local socket address
 
-	if( bind(socketHandle, (struct sockaddr *) &socketInfo, sizeof(socketInfo)) < 0)
-	{
+	if (bind(socketHandle, (struct sockaddr *) &socketInfo, sizeof(socketInfo))
+			< 0) {
 		close(socketHandle);
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
 
-	if(listen(socketHandle, 5) == -1) {
+	if (listen(socketHandle, 5) == -1) {
 		return -1;
 	}
-	clientMsj mensaje = {};
 
-	while(1) {
+	//arranco el thread que va a ir desencolando los mensajes que recibe el server
+	pthread_t messageReaderThread;
+	pthread_create(&messageReaderThread, NULL, readMsjs, &msjs);
+
+	while (1) {
+		clientMsj msj = { };
 		int socketConnection = accept(socketHandle, NULL, NULL);
+		int rc = readBlock(socketConnection, &msj, 44);
+		cout << msj.id << endl;
+		msjs.push_back(msj);
+		cout << msjs.size() << endl;
 
-		int rc = readBlock(socketConnection, &mensaje, 44);
-		cout << "Number of bytes read: " << rc << endl;
-		cout << "id recibido: " << mensaje.id << endl;
-		cout << "tipo de mensaje recibido: " << mensaje.type << endl;
-		cout << "valor recibido: " << mensaje.value << endl;
 	}
+	pthread_join(messageReaderThread, NULL);
 	close(socketHandle);
 }
