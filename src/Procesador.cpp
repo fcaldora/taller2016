55fc076b9466a7f91c608c8e09e90fa59fcda5fe
pabloisrael <pabloisrael94@gmail.com>
2016-06-05 10:59:26 -0300

    User can turn off the sound of the other users

--- src/Procesador.cpp
+++ src/Procesador.cpp
@@ -89,7 +89,7 @@ void Procesador::processShootMessage(clientMsj message) {
 
 	Object *bullet = this->gameManager->createBulletForClient(client);
 	mensaje response = MessageBuilder().createBulletMessage(bullet);
-	mensaje soundMessage = MessageBuilder().createBulletSoundMessage();
+	mensaje soundMessage = MessageBuilder().createBulletSoundMessage(message);
 	this->gameManager->broadcastMessage(response);
 	this->gameManager->broadcastMessage(soundMessage);
 }
